12f6818e7d602d222d037952370f2dc0415814fb
achkamal <ach.khamal@gmail.com>
2026-04-21 15:59:20 +0700

    feat: add useEfect for auto fetch post data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,19 @@ import { useAppContext } from '../../context/AppContext';
 import CreatePost from '../../components/CreatePost/CreatePost';
 import PostCard from '../../components/Post/PostCard';
 import './Home.css';
+import { useEffect } from 'react';
 
 const Home = () => {
-  const { posts, addNewPost, loading, error } = useAppContext();
-
+  const { fetchPosts, posts, addNewPost, loading, error } = useAppContext();
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   return (
     <div className="home-container">
       {error && <div className="error-banner">{error}</div>}
       {loading && posts.length === 0 && <div className="loading-overlay">Loading...</div>}
       <CreatePost onPost={addNewPost} />
-      
+
       <div className="feed">
         {posts.length > 0 ? (
           posts.map(post => (
